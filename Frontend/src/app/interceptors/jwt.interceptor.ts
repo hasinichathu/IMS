4d7c25e6aa1805ad4862ac6b2f37dfc6460ab0ee
hasinichathu <164124V@uom.lk>
2020-06-11 05:37:14 +0530

    Modification of jwt intercepter

--- Frontend/src/app/interceptors/jwt.interceptor.ts
+++ Frontend/src/app/interceptors/jwt.interceptor.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
-  HttpResponse,
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map, catchError } from 'rxjs/operators'
 import { AuthenticationService } from '../auth/authentication.service';
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
@@ -18,32 +15,19 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // add auth header with jwt if user is logged in and request is to api url
         const currentUser = this.authenticationService.currentUserValue;
-        const isLoggedIn = currentUser && currentUser.token;
+        const isLoggedIn = currentUser && currentUser.accessToken;
         if (isLoggedIn) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
+                    Authorization: `Bearer ${currentUser.accessToken}`
                 }
             });
+            
         }
         if (!request.headers.has('Content-Type')) {
           request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
       }
+      console.log(request);
       return next.handle(request);
-    //   return next.handle(request).pipe(
-    //     map((event: HttpEvent<any>) => {
-    //         if (event instanceof HttpResponse) {
-    //             catchError((error: HttpErrorResponse) => {
-    //                 let data = {};
-    //                 data = {
-    //                     reason: error && error.error.reason ? error.error.reason : '',
-    //                     status: error.status
-    //                 };
-    //                 // this.errorDialogService.openDialog(data);
-    //                 return throwError(error);
-    //             })
-    //         }
-    //         return event;
-    //     }));
   }
 }
