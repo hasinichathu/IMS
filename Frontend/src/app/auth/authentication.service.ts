871770f83ee441527b6a5780e33867969a802f5c
hasinichathu <164124V@uom.lk>
2020-07-19 03:59:25 +0530

    folder structure changes

--- Frontend/src/app/auth/authentication.service.ts
+++ Frontend/src/app/auth/authentication.service.ts
@@ -4,7 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
-import { User } from '../models/user.model;
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
