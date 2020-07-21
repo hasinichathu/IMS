7f54c83fc422710e1ff699b649b5e014970ed526
Hasini Chathurangi <hasini.16@itfac.mrt.ac.lk>
2020-07-22 03:41:58 +0530

    Update Frontend/src/app/layouts/admin-layout/pages/batch/batch.component.ts
    Co-authored-by: yashodgayashan <32588172+yashodgayashan@users.noreply.github.com>

--- Frontend/src/app/layouts/admin-layout/pages/batch/batch.component.ts
+++ Frontend/src/app/layouts/admin-layout/pages/batch/batch.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Batch } from '../../../../models/batch.model';
 import { BatchService } from '../../../../services/batch.service';
+
 @Component({
   selector: 'app-batch',
   templateUrl: './batch.component.html',
