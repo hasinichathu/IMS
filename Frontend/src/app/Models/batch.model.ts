f941ebd3d9909e30e9a0692552e75eb11311e41d
hasinichathu <164124V@uom.lk>
2020-07-23 18:48:18 +0530

    batch model

--- Frontend/src/app/Models/batch.model.ts
+++ Frontend/src/app/Models/batch.model.ts
@@ -1,12 +1,4 @@
 export class Batch {
-
-    // BatchId: string;
-    // CreatedBy: string;
-    // Year: number;
-    // StartDate: string;
-    // EndDate: string;
-    // CompanyCount: number;
-
     data: [
         {
             BatchId: string,
